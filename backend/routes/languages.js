4efcc681063e9ec58eb450320b80b9e8c6360dd7
Noorae~ <noora_vain@hotmail.com>
2024-01-05 19:08:15 +0200

    languages.js: Fix req.query to req.params.

--- backend/routes/languages.js
+++ backend/routes/languages.js
@@ -6,7 +6,7 @@ const languagesRouter = express.Router();
 languagesRouter.get("/:language", async (req, res) => {
   try {
     //extract the language from the url
-    const language = req.query.language;
+    const language = req.params.language;
     const translations = await database.findAll(language);
     res.json(translations);
   } catch (err) {
