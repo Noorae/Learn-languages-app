510f8a84af1ce84d5ad4cb60d5cdfa5c09ad48ce
Noorae~ <noora_vain@hotmail.com>
2024-01-05 18:56:34 +0200

    languages.js: Add parameter to get all function.

--- backend/routes/languages.js
+++ backend/routes/languages.js
@@ -3,11 +3,15 @@ const express = require("express");
 const languagesRouter = express.Router();
 // to do: validation
 
-languagesRouter.get("/", async (req, res) => {
+languagesRouter.get("/:language", async (req, res) => {
   try {
-    const translations = await database.findAll();
+    //extract the language from the url
+    const language = req.query.language;
+    const translations = await database.findAll(language);
     res.json(translations);
   } catch (err) {
     res.status(500).json({ msg: err.message });
   }
 });
+
+module.exports = languagesRouter;
