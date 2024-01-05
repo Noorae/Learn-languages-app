4160d427e10708f15ccfecdf6b1fdde4292589bf
Noorae~ <noora_vain@hotmail.com>
2024-01-05 19:13:33 +0200

    index.js: Add server connection and shutdown functions.

--- backend/index.js
+++ backend/index.js
@@ -11,5 +11,44 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/languages/english", languagesRouter);
-app.use("api/languages/swedish", languagesRouter);
-app.use("api/languages/korean", languagesRouter);
+app.use("/api/languages/swedish", languagesRouter);
+app.use("/api/languages/korean", languagesRouter);
+
+const server = app
+  .listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  })
+  .on("error", (err) => {
+    console.error("Error starting server:", err);
+    process.exit(1);
+  });
+
+const gracefulShutdown = () => {
+  console.log("Starting graceful shutdown...");
+  // Close the server
+  if (server) {
+    console.log("Server was opened, so we can close it...");
+    server.close((err) => {
+      if (err) {
+        console.error("SERVER: Error closing Express server: ", err);
+      } else {
+        console.log("SERVER stopped.");
+      }
+
+      console.log("MYSQL Starting graceful shutdown....");
+      pool.end((err) => {
+        if (err) {
+          console.error("MYSQL: Error closing MYSQL connection", err);
+        } else {
+          console.log("MYSQL: Connection closed. ");
+        }
+
+        console.log("MYSQL: Shutdown complete.");
+        process.exit(1);
+      });
+    });
+  }
+};
+
+process.on("SIGTERM", gracefulShutdown); // Some other app requirest shutdown.
+process.on("SIGINT", gracefulShutdown); // ctrl-c
