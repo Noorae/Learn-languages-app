a5122860b31ecb86425891625e24fb98a5efe7d6
Noorae~ <noora_vain@hotmail.com>
2024-01-05 18:46:40 +0200

    languages.js: Add get function to fetch all values.

--- backend/routes/languages.js
+++ backend/routes/languages.js
@@ -2,3 +2,12 @@ const database = require("../database/databaseFunctions.js");
 const express = require("express");
 const languagesRouter = express.Router();
 // to do: validation
+
+languagesRouter.get("/", async (req, res) => {
+  try {
+    const translations = await database.findAll();
+    res.json(translations);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+});
