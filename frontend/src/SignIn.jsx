87c79961eb4c60adde25931948f6d544b957fad0
Noorae~ <noora_vain@hotmail.com>
2024-01-13 22:02:52 +0200

    Edit: SignIn.jsx: remove unneseccary components.

--- frontend/src/SignIn.jsx
+++ frontend/src/SignIn.jsx
@@ -89,10 +89,6 @@ export default function SignIn() {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
             <Button
               type="submit"
               fullWidth
@@ -101,12 +97,7 @@ export default function SignIn() {
             >
               Sign In
             </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href="#" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
+            <Grid container justifyContent="flex-end">
               <Grid item>
                 <Link href="/signup" variant="body2">
                   {"Don't have an account? Sign Up"}
