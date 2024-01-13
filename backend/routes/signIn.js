ab453164ed40fb6b42b61b81340fa923be576f41
Noorae~ <noora_vain@hotmail.com>
2024-01-13 22:27:15 +0200

    signIn.js: Add get route function for sign ins.

--- backend/routes/signIn.js
+++ backend/routes/signIn.js
@@ -1,3 +1,20 @@
 const database = require("../database/databaseFunctions.js");
 const express = require("express");
-const languagesRouter = express.Router();
+const signInRouter = express.Router();
+
+signInRouter.get("/:myUserName", async (req, res) => {
+  try {
+    //extract username from the url
+    const username = req.params.myUserName;
+    console.log(username);
+    //extract password from req
+    //hash the password
+    //check if there are matches in the database
+    //TO DO: const user = await database. FUNCTION THAT FINDS CORRECT ID/USER;
+    //response with the right data
+    //CREATE JWS TOKEN
+    res.json(translations);
+  } catch (err) {
+    res.status(500).json({ msg: err.message });
+  }
+});
