98a75413d67a6c8bd761c611950be17a5a460cda
Noorae~ <noora_vain@hotmail.com>
2024-01-14 16:25:09 +0200

    Add: Api.jsx: Add postData function.

--- frontend/src/Api.jsx
+++ frontend/src/Api.jsx
@@ -11,3 +11,25 @@ export const fetchData = async (url) => {
     throw error;
   }
 };
+
+export const postData = async (url, data) => {
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    console.log("Error while posting data:", error);
+    throw error;
+  }
+};
