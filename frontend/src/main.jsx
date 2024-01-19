93cd0a17d47b141c237769520acef34a97fb0991
Noorae~ <noora_vain@hotmail.com>
2024-01-19 17:07:36 +0200

    Add: main.jsx: JSDOC comments.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./AuthContext";
 
+/**
+ * Render the root for the application.
+ * @function
+ *
+ */
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
